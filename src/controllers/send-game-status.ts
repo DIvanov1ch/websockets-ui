52be7628e9e71058368a83b623ce8e8d566c4b2a
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-21 00:33:51 +0300

    refactor: rename send game function

--- src/controllers/send-game-status.ts
+++ src/controllers/send-game-status.ts
@@ -1,5 +1,5 @@
 import { Players_DB } from '../data-bases/players.db';
 
-export const sendGame = (playerId: number, response: string) => {
+export const sendGameStatus = (playerId: number, response: string) => {
   Players_DB.getPlayerById(playerId)!.ws.send(response);
 };
