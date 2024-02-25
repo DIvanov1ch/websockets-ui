994a6255f67ea753cf08ad2e821b46c303e77899
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-25 20:02:21 +0300

    refactor: move create datas to get response

--- src/services/create-rooms-data.ts
+++
@@ -1,7 +0,0 @@
-import { stringify } from "../utils/stringify";
-import { Rooms_DB } from "../data-bases/rooms.db";
-
-export const createRoomsData = () => {
-  const rooms = Rooms_DB.getAllRooms();
-  return stringify(rooms);
-}
--- src/services/create-turn-data.ts
+++
@@ -1,9 +0,0 @@
-import { stringify } from '../utils/stringify';
-import { Game } from '../models/game.model';
-
-export const createTurnData = (game: Game) => {
-  const { turn } = game;
-  const turnData = stringify({ currentPlayer: turn });
-
-  return turnData;
-};
