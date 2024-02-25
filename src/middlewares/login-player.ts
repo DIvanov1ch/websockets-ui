a4f8ef549516488d17ae73c7f2d24242035af52e
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-25 20:40:46 +0300

    refactor: update login player function

--- src/middlewares/login-player.ts
+++ src/middlewares/login-player.ts
@@ -5,22 +5,24 @@ const alreadyLoggedIn = 'User already logged in';
 
 const incorrectCredentials = 'Username or password you entered is incorrect';
 
+const setLoginError = (player: Player, isError: boolean, text: string) => {
+  player.error = isError;
+  player.errorText = text;
+};
+
 export const loginPlayer = (
   player: Player,
   password: string,
   ws: WebSocket,
 ) => {
   if (player.password !== password) {
-    player.error = true;
-    player.errorText = incorrectCredentials;
+    setLoginError(player, true, incorrectCredentials);
     return;
   }
   if (player.ws !== null) {
-    player.error = true;
-    player.errorText = alreadyLoggedIn;
+    setLoginError(player, true, alreadyLoggedIn);
     return;
   }
   player.ws = ws;
-  player.error = false;
-  player.errorText = '';
+  setLoginError(player, false, '');
 };
